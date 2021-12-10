0d09afe0a3c7f1502be9383762e375c95045ec1c
Vedaant Arya (V3D3) <vedaant.arya.v3d3@gmail.com>
2021-12-10 23:36:45 +0530

    Added some useful methods to generator, created a model of how the output NoC module should look like

---
+++ Project3/_noc_model.bsv
@@ -0,0 +1,32 @@
+package noc;
+
+import Vectors::*;
+import toplevel_defs::*;
+import GetPut::*;
+
+// example for:
+// L1 C,3,1
+// L2 all H,8,8
+
+import temps/chain_l1_hypercube_l2::*;
+
+module noc (Empty);
+    Ifc_core c0_0 <- mkCore;
+    Ifc_node n0_0 <- hypercube_l2#(3, Node_addr {L1_headID: 0, L2_ID: 0}, c0_0, 0, 1, 2);
+    ... for c0_1, n0_1... upto c0_7, n0_7
+    ... then for c1_0, n1_0... upto c7_7, n7_7
+
+    // now make connections
+    mkConnection(n0_0.node_channels[2], n0_1.node_channels[2]);
+    mkConnection(n0_0.node_channels[1], n0_2.node_channels[1]);
+    mkConnection(n0_0.node_channels[0], n0_4.node_channels[0]);
+    ... for n0_i with bit differences from i...
+
+    // now make L1 connections
+    mkConnection(n0_0.node_channels[3], n1_0.node_channels[3]);
+    mkConnection(n1_0.node_channels[4], n2_0.node_channels[3]);
+
+    // simulate?
+endmodule: noc
+
+endpackage: noc
--- Project3/generator.py
+++ Project3/generator.py
@@ -1,4 +1,6 @@
-def requirefile(file):
+import re
+
+def requirefile(file, user=False):
     try:
         f = open(file)
         out = f.read()
@@ -6,8 +8,43 @@ def requirefile(file):
         return out
     except OSError:
         print('Failed to open ' + f)
+        if(user):
+            print('The file is required as input. Kindly refer to README')
+        else:
+            print('The file is required for synthesis. This copy of the program is corrupted, kindly obtain a new copy from Team PraShaAnt (Shashank, Vedaant, Praneeth), IIT Madras (CS6230 JN21).')
         exit()
 
-requirefile('core.bsv')
-L1T = requirefile('L1Topology.txt')
-L2T = requirefile('L2Topology.txt')
+requirefile('core.bsv', True)
+requirefile('toplevel_defs.bsv')
+requirefile('noc_template_head.bsv')
+requirefile('noc_template_tail.bsv')
+L1T = requirefile('L1Topology.txt', True)
+L2T = requirefile('L2Topology.txt', True)
+
+# convert a definitions file (for L1) to an array of definitions
+def f2d(filename):
+    f = open(filename)
+    d = f.read().split('$$$')
+    f.close()
+
+    return d
+
+# plug in the definitions for L1 into given src
+def fit(src, defs):
+    return re.sub('$$(\d)', lambda matchObj : defs[int(matchObj.groups()[0])], src)
+
+####
+# Begin parsing the Topologies.
+L2T = L2T.split('\n')
+
+def parseTLine(line):
+    line = line.split(',')
+    return (line[0], int(line[1]), int(line[2]))
+
+L1T = parseTLine(L1T)
+L2T = [parseTLine(i) for i in L2T]
+
+out = ''
+def genout(s):
+    out += s
+
